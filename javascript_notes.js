0e63460756ace42c190e76ca05da007e35f7cc8a
srini88 <pvsrinivas.ece@gmail.com>
2016-01-04 10:47:12 -0600

    some more notes on timeout and clear

--- javascript_notes.js
+++ javascript_notes.js
@@ -1,2 +1,37 @@
 lets you add behaviour to webpages
 
+http://developer.telerik.com/featured/a-guide-to-javascript-engines-for-idiots/
+
+JavaScript is loosely typed (you don’t have to define the datatype of variables) while Java is not. JavaScript is mainly executed in a browser, where as Java can be used to create stand alone applications. Java has a much wider area of application as compared to JavaScript which largely lives inside the browser.
+
+JavaScript is Object Oriented but unlike other programming language that implement inheritance via classes, JavaScript does so via prototypes. 
+
+A JavaScript engine is a virtual machine[citation needed] which interprets and executes JavaScript. Although there are several uses for a JavaScript engine, it is most commonly used in web browsers
+
+A ‘JavaScript engine’ is often termed a type of virtual machine. A ‘virtual machine’ refers to the software-driven emulation of a given computer system. There are many types of virtual machines, and they are classified by how precisely they are able to emulate or substitute for actual physical machines.
+
+The basic job of a JavaScript engine, when all is said and done, is to take the JavaScript code that a developer writes and convert it to fast, optimized code that can be interpreted by a browser or even embedded into an application. JavaScriptCore, in fact, calls itself an “optimizing virtual machine”.
+
+More precisely, each JavaScript engine implements a version of ECMAScript, of which JavaScript is a dialect. As ECMAScript evolves, so do JavaScript engines. The reason there are so many different engines is each one is designed to work with a different web browser, headless browser, or runtime like Node.js.
+
+Google’s V8 engine, written in C++, also compiles and executes JavaScript source code, handles memory allocation, and garbage collects leftovers. Its design consists of two compilers that compile source code directly into machine code:
+
+
+with modern browsers,   with javascript you now have the conveniences of a scripting language while enjoing the perfomrance of a compiled language 
+
+
+ecma script serves as the standard language definition for all javascipt mplementations 
+
+ECMAScript is a trademarked[1] scripting language specification standardized by Ecma International in ECMA-262 and ISO/IEC 16262. Well-known implementations of the language, such as JavaScript, JScript and ActionScript are widely used for client-side scripting on the Web.
+
+
+JavaScript is prototype-based with first-class functions, making it a multi-paradigm language, supporting object-oriented,[8] imperative, and functional programming styles.
+
+It has an API for working with text, arrays, dates and regular expressions, but does not include any I/O, such as networking, storage or graphics facilities, relying for these upon the host environment in which it is embedded.
+
+JavaScript uses prototypes where many other object-oriented languages use classes for inheritance.[41] It is possible to simulate many class-based features with prototypes in JavaScript
+
+
+with javascript you write statements , each statement specifies a small part of a computation and together all the sttaements create the behaviour of th epage
+
+loops in js: while, for, for in , forEach 
--- js.js
+++ js.js
@@ -11,6 +11,10 @@ setTimeout(callMe, 3000);
 function callMe(){
 	alert("sup my nigga");
 }
+// var id = setTimeout(fn, delay); – Initiates a single timer which will call the specified function after the delay. The function returns a unique ID with which the timer can be canceled at a later time.
+
+var id = setInterval(fn, delay); – Similar to setTimeout but continually calls the function (with a delay every time) until it is canceled.
+clearInterval(id);, clearTimeout(id); – Accepts a timer ID (returned by either of the aforementioned functions) and stops the timer callback from occurring.
 
 
 // The window.setTimeout() method can be written without the window prefix.
