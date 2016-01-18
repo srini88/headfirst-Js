e1a0a7da95b7ca063142789840e80aae1238fdc3
srini88 <pvsrinivas.ece@gmail.com>
2016-01-18 16:22:47 -0600

    working with objects - start

--- battleship.js
+++ battleship.js
@@ -1,67 +1,124 @@
-// battle ship puzzle 
-// first place ship in 3 locations 
-// we have 1D grid from 0 to 6
-
-var loc1 = 1;
-var loc2 = 2;
-var loc3 = 3;
-var isSunk = false;
-var guess;
-var hits = 0;
-var guessCount = 0;
-
-while (isSunk==false){
-// now start the application
-// ask the user for input
-
-guess = prompt("give your guess of the battleship between 0 to 6");
-
-if (guess <0 || guess >6){
-	alert("use between 0 to 6, enter again");
-}else{
-	guessCount = guessCount+1;
-	if (guess ==loc1 || guess ==loc2 || guess ==loc3) 
-	{
-		hits = hits+1;
-		alert("hit");
-		if (hits == 3){
-			isSunk = true;
-			alert("you sunk my battleship");
-		}
-	}
-	else
-		alert("miss");
-}
+// // sparce array 
+// var arr1 = [2, 4, 5];
+// arr1[9] = 10;
 
 
-}
-alert("your guess count probability" + (3/guessCount));
-// var location1 = 3;
-// var location2 = 4;
-// var location3 = 5;
-// var guess;
-// var hits = 0;
-// var guesses = 0;
-// var isSunk = false;
+// // to check for undefined and priint onoly the items that are present 
+// for (var i = 0; i <= arr1.length ; i++){
 
-// while (isSunk == false) {
-// 	guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-// 	if (guess < 0 || guess > 6) {
-// 		alert("Please enter a valid cell number!");
-// 	} else {
-// 		guesses = guesses + 1;
-// 		if (guess == location1 || guess == location2 || guess == location3) {
-// 			alert("HIT!");
-// 			hits = hits + 1;
-// 			if (hits == 3) {
-// 				isSunk = true;
-// 				alert("You sank my battleship!");
-// 			}
-// 		} else {
-// 			alert("MISS");
-// 		}
+// 	if (arr1[i] != undefined){
+// 		console.log(arr1[i]);
 // 	}
 // }
-// var stats = "You took " + guesses + " guesses to sink the battleship, " +
-//             "which means your shooting accuracy was " + (3/guesses);
-// alert(stats);
+
+// another way to create an array new Array
+
+// var arr2 = new Array(5);
+// arr2.push(4);
+// console.log(arr2);  // prints [5: 4]  - 5 th key value 4
+
+
+// var arr3 = [];
+
+// console.log(arr3);
+
+// array literal notation is where you define a new array using just empty brackets. In your example:
+
+// var myArray = [];
+// var a = [],            // these are the same
+//     b = new Array(),   // a and b are arrays with length 0
+
+//     c = ['foo', 'bar'],           // these are the same
+//     d = new Array('foo', 'bar'),  // c and d are arrays with 2 strings
+
+//     // these are different:
+//     e = [3],             // e.length == 1, e[0] == 3
+//     f = new Array(3);   // f.length == 3, f[0] == undefined
+
+
+// working with objects 
+
+
+// var car = {
+// 	make:"infiniti",
+// 	model : "Q50",
+// 	year : 2015
+// };
+
+
+// console.log(car); //Object {make: "infiniti", model: "Q50", year: 2015}
+// console.log(car.make);
+
+
+// // in oo we thingk about a problem in objects 
+// // difference between making a toaster and using a toaster object 
+
+// // adding a new property
+
+// car.type = "gasoline"; 
+// console.log(car);  //Object {make: "infiniti", model: "Q50", year: 2015, type: "gasoline"}
+
+// car.type = "electric";// Object {make: "infiniti", model: "Q50", year: 2015, type: "electric"}
+// console.log(car);
+
+
+// // to delete a property  use delete keyword
+
+// delete car.type;  // when you are deleting a property, you are not just deleting the value of the property , you are deleting the property itself 
+// console.log(car);  //Object {make: "infiniti", model: "Q50", year: 2015}
+
+
+// // the result of the expression car.type will be undefined if car does nto have a property named type 
+
+// console.log(car.type);  //undefined since we already deleted 
+
+// variables dont hold objects , they hold reference to an obj 
+
+// pass by value is pass sby copy 
+
+
+// passing objects to and fro
+var car = {
+	make:"infiniti",
+	model : "Q50",
+	year : 2015
+};
+
+// function passOO(car){
+
+// 	car.type = "electric";
+// };
+
+// var car2 = car;
+// console.log(car);   // both same
+// console.log(car2);  // both same 
+
+// passOO(car);
+
+// // so if you change a property of the object in a function, you are changing the property in the original object 
+// console.log(car);     // both got changed , evenn though I changed only car 
+// console.log(car2);
+car.drive =  function() {
+	console.log("car is being driven");
+};
+// tghe above fn added a ton of stuff to our objec t
+
+// drive: ()
+// arguments: null
+// caller: null
+// length: 0
+// name: ""
+// prototype: car.drive
+// __proto__: ()
+// <function scope>
+// make: "infiniti"
+// model: "Q50"
+// year: 2015
+// __proto__: Object
+
+//console.log(car);
+
+console.log(car);
+
+
+car.drive();  //car is being driven
--- test.js
+++ test.js
@@ -1,136 +1,80 @@
-// var isTrue = true;
-// var status = (isTrue==false);  //true ==false results in false and gets assigned to status 
+// javascript array methods 
 
-// console.log(status); //prints false
+// Converting Arrays to Strings
+// The JavaScript method toString() converts an array to a string of (comma separated) array values.
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// function doIt(param){
-// 	param=2;
-// 	console.log(param);   // param gets assigned 2 , before it was five when we passed
-// }
+// var str = fruits.toString();
+// console.log(str);
 
-// var test=5;
-// doIt(test); 
-// //console.log(test);
+//Banana,Orange,Apple,Mango
 
+// The join() method also joins all array elements into a string.
 
-// function makeTea (cups, people){
-// 	console.log("making tea with cups: " + cups + " for people: "+ people);
-// }
+// It behaves just like toString(), but in addition you can specify the separator:
 
-// makeTea(2,3); //making tea with cups: 2 for people: 3
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// makeTea(2); //making tea with cups: 2 for people: undefined
+// // var str = fruits.join("*"); // same as below 
+// var str = fruits.join('*');
+// console.log(str);
 
 
-// makeTea (2, "nigga", 5);  //making tea with cups: 2 for people: nigga, ignores the extra arguments 
-// calculating area of circle
+// When you work with arrays, it is easy to remove elements and add new elements.
 
-// function areaCircle (r){
-// 	var area = Math.PI * r * r;   // used Math.PI
-// 	return area;
-// }
+// This is what popping and pushing is:
 
-// var calc = areaCircle(2);
-// console.log(calc);
+// Popping items out of an array, or pushing items into an array.
+// The pop() method removes the last element from an array:
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// // dont need to have var keyword infront of it explicitly, the function  does the work 
+// fruits.push("kiwi");
 
-// // global variables life ends as the page goes away, even if you reload the same page, all your global variables are destryoed and then recreated intehh newly loadaded page 
+// console.log(fruits); //["Banana", "Orange", "Apple", "Mango", "kiwi"]
+// fruits.pop();
+// console.log(fruits);  //["Banana", "Orange", "Apple", "Mango"]
 
-// points = 0 ;  by default makes it global  -- since no var in front
+//The push() method returns the new array length:
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// var pvs = 5;
-// function changePvs (){   // you are shadowing your golbal , any references to pvs within the function refer to the local variable and not the global 
-// 	// global variable is in the shadow of the local variable , we cannt see global because local version is in our way 
-// 	var pvs = 3;
-// 	console.log(pvs);
-// }
-// changePvs();
-// console.log(pvs);
-// // prints 3 and 5 
+// var x = fruits.push("jackfruit");
+// console.log(x);  // prints 5
 
-// Math.random()  function attached to an object
+// Shifting is equivalent to popping, working on the first element instead of the last.
 
-// primitive type - strings numbers and booleans 
+// The shift() method removes the first element of an array, and "shifts" all other elements one place up.
 
-// var arr = ["vanilla", "butter", "pecans", "chocolate"];
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// console.log(arr.length);
+// fruits.shift();
+// console.log(fruits);  //["Orange", "Apple", "Mango"]
 
 
-// var arr = ["vanilla", "butter", "pecans", "chocolate"];
-
-// console.log(arr);'
-
-// var arr1 = ["a", "b", "c"];
-// var arr2  = [1, 2, 3];
-
-// function rando(){
-
-// 	var ran1 = Math.floor(Math.random()* arr1.length);
-// 	var ran2 = Math.floor(Math.random()* arr2.length);
-// 	var phase = arr1[ran1] + arr2[ran2];
-// 	//alert(phase);	
-// 	return phase;
-// }
-
-// console.log(rando());  // prints a1, b3, etc...
-
-//all the values in the array need not be same tyope
-
-// console.log(arr1[5]);    - you get undefined
-
-// printing array using forloop
-// var arr2  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// function printArr(){
-
-// 	for (var i=0; i <arr2.length-1 ; i++){
-// 		console.log(arr2[i]);
-
-// 	}
-
-// }
-
-// printArr();
+// The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
 
 
-// printing array using while loop 
-// var arr2  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// function printArr(){
+// fruits.unshift("kiwi");
+// console.log(fruits);  //["kiwi", Orange", "Apple", "Mango"]
 
-// 	var i = 0;
+// The shift() method returns the string that was "shifted out".
 
-// 	while(i < arr2.length){
-// 		console.log(arr2[i]);
-// 		i++;
-// 	}
+// The unshift() method returns the new array length.
 
 
-// }
+// Since JavaScript arrays are objects, elements can be deleted by using the JavaScript operator delete:
+// Using delete on array elements leaves undefined holes in the array. Use pop() or shift() instead.
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// printArr();
-// var arr2  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// delete fruits[0];
+// console.log(fruits); //[1: "Orange", 2: "Apple", 3: "Mango"]
+// console.log(fruits[0]);  //undefined
 
-// var i=0;
-// while (arr2[i] == 3) {    // wont even go into this bcoz the first condition itself is false 
-// 	console.log(arr2[i]); 
-// 	i=i+1;	
-// 	if (i == 6) break;
-// }
+// need to learn more js functions
 
 
-var arr2  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var i=0;
-while (arr2[i] != 3) {    // wont even go into this bcoz the first condition itself is false 
-	console.log(arr2[i]); 
-	i=i+1;	
-	//if (i == 6) break;
-}
-
-//prints 1 and 2; 
 
 
 
