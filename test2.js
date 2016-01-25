f55a97d391f6ebbc5393857c71f1759ee58a2ccf
srini88 <pvsrinivas.ece@gmail.com>
2016-01-24 19:34:02 -0600

    window onresize

--- test2.js
+++ test2.js
@@ -1,30 +1,6 @@
+function resize (){
 
-window.onload = init;
-
-function init(){
-
-	var images = document.getElementsByTagName("img");
-
-	for (var i =0; i < images.length ; i++){
-		images[i].onmouseover  = changeImg;
-	}
+	var id = document.getElementById("resizeId");
+	id.innerHTML = "resized";
 }
-
-function changeImg(eventObj){
-	// get single image
-	var img = eventObj.target;
-	//var name = img.src;
-	//console.log(name); //http://localhost/rejuvenate/headfirst-Js/one.jpg
-	var name = img.id;
-	name = name + "blur.jpg";
-	img.src = name;
-	img.onmouseout = reblur;
-	setTimeout(reblur, 3000, img);   // im passing img to figure out what image to be reverted back  =  3rd attribute 
-}
-
-function reblur(eventObj){
-	var img = eventObj.target;
-	var id = img.id;
-	var name = id+".jpg";
-	img.src = name;
-}
+window.onresize = resize;
