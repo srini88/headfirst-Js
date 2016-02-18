7d3c00f68837da8793e55ed5809531d7cf9be46c
srini88 <pvsrinivas.ece@gmail.com>
2016-02-17 21:15:28 -0600

    closure starting 1

--- test4.js
+++ test4.js
@@ -1,37 +1,62 @@
 
-//fly();  //im getting vUncaught TypeError: fly is not a functio
+function foo(){
+	console.log("logging foo");
 
 
-var fly = function() {
-	console.log("I'm flying");
+	function bar(){
+		console.log("loggin bar");
+	}
+	return bar;
+}
+
+
+ var something = foo();  // causes logging foo to get printed on screen and something gets bar 
+
+
+// console.log(something);   //which prints function bar(){
+// 		//console.log("loggin bar");
+// 	//}
+
+something(); // you run something which runs bar ()  (this is the final closure)
+
+
+// closure = function + outer context   (variable this function relies on )
+
+// (the function is usually created/returned by another function)
+
+// A real-life closure
+// A closure is a function with access to variables in its containing scope (the function “closes over” the variables). The thing that can be tricky to wrap your head round is that the inner function still has access to the outer function’s variables after the outer function has returned
+
+function outer(){
+
+	var counter = 3;
+	console.log(counter);
+
+	function inner(){
+		counter = counter+1; 
+		console.log(counter);
+	}
+
+	return inner;
 }
 
 
 
+var x = outer();  // prints 3
 
-// So what does this all mean? For starters, it means that you can place function
-// declarations anywhere in your code—at the top, at the bottom, in the middle—and
-// invoke them wherever you like. Function declarations create functions that are defined
-// everywhere in your code (this is known as hoisting).
-
-
-// Here’s the
-// short answer: the only difference between a function defined at the top level of your code and
-// one that’s defined within another function is just a matter of scope. In other words, placing a
-// function in another function affects where the function is visible within your code.
+x();  //4
+x(); //5
+// In this code, outer is called once, and returns inner. x is a reference to inner. Because inner is a closure, it has access to outer‘s local variable, counter. Even though outer has returned, inner still has access to outer‘s variables. Be careful though – if outer were called again, we’d get a new version of inner. To continue the previous example:
 
 
-// review of lexical scope 
+// In JavaScript, using simple terms,  if we create a function inside another function, we are creating a closure. 
 
-var justAVar = "global";
+// In most common languages, after a function returns, all the local variables are no longer accessible because the stack-frame is destroyed.  Keeping this in mind a closure can be considered as a stack-frame which is not deallocated when the function returns. 
 
 
-function WhereAreYou(){
-	var justAVar = "local";
-	return justAVar;    // returns local 
-}
+var y = outer();  // prints 3
 
-var result = WhereAreYou();
-console.log(result);  // you get local here 
+x();  //6
+x();   //7
 
-//492
+y();  //4
